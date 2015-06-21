165a3f5760b034c230c9b41347e10ffe3abc3725
GauthamSundar <gautham.sundar@outlook.com>
2015-06-21 22:11:31 +0530

    corrected minor mistake in get matrix inverse function in cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,33 @@
 ## File includes two R functions: 1. makeCacheMatrix & 2. cacheSolve
-## The function makeCacheMatrix returns a list containing 4 functions: 1. setMat 
+
+
+## The makeCacheMatrix function. makeCacheMatrix returns a list containing 4 functions: 1. setMat 
 ## 2. getMat 3. setMatInv & 4. getMatInv.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x) {            ## Function makeCacheMatrix accepts a matrix x
-        MatInv <- NULL                      ## Initializing value for future reference
-        setMat <- function(y) {             ## setMat assigns the value of a new matrix y to x 
+makeCacheMatrix <- function(x) {                ## Function makeCacheMatrix accepts a matrix x
+        MatInv <- NULL                          ## Initializing inverse value for future reference
+        setMat <- function(y) {                 ## setMat accepts a matrix y as argument and assigns its value to x 
                 x <<- y
-                MatInv <<- NULL
+                MatInv <<- NULL			## sets new matrix inverse to NULL
         }
-        getMat <- function() x              ## getMat prints value of x
-        setMatInv <- function(MI) MatInv <<- MI ## setMatInv assigns the matrix inverse of x i.e. MI calculated by function cacheSolve to MatInv 
-        getMatInv <- function() MatInv        ## getMatInv prints the matrix inverse of x, MatInv
-        list(setMat = setMat, getMat = getMat, ## Each function is assigned the same name as itself within the list for convenience 
+        getMat <- function() x                  ## getMat returns matrix x
+        setMatInv <- function(MI) MatInv <<- MI ## setMatInv accepts matrix inverse i.e. MI as argument and assigns it to variable MatInv 
+        getMatInv <- function() MatInv          ## getMatInv returns the matrix inverse, MatInv
+        list(setMat = setMat, getMat = getMat,  ## Each function is assigned the same name as itself within the list for convenience when calling the function
              setMatInv = setMatInv,
              getMatInv = getMatInv)
 }
 
-## Write a short comment describing this function
 
-cacheSolve <- function(u) {                  ## Function cacheSolve accepts above defined list, u
-        cachedMatInv <- u$getMatInv()        ## assign MatInv, which may or may not have been calculated already,to cache variable cachedMatInv
-        if(!is.null(cachedMatInv)) {         ## verify if MatInv was calculated 
-                cat("getting cached data...\n") ## print message  
+## The cacheSolve function. Accepts the above list as argument and calculates matrix inverse if value not already cached.
+
+cacheSolve <- function(u) {                     ## Function cacheSolve accepts above defined list, u as argument
+        cachedMatInv <- u$getMatInv()           ## assign MatInv from above function, which may or may not have been calculated already,to cache variable cachedMatInv
+        if(!is.null(cachedMatInv)) {            ## verify if MatInv was calculated previously 
+                cat("getting cached data...\n") ## print message that inverse has been cached previously  
                 return(cachedMatInv)            ## return cached value of MatInv
         }
-        data <- u$get()                      ## If MatInv has not been calculated, retrieve matrix x 
-        calMatInv <- solve(data)             ## Calculate matrix inverse of x and assign to variable calMatInv
-        u$setMatInv(calMatInv)               ## return matrix inverse CalMatInv
-        
+        data <- u$getMat()                      ## If MatInv has not been calculated, retrieve matrix to perform matrix inverse calculation 
+        calMatInv <- solve(data)                ## Calculate matrix inverse and assign to variable calMatInv
+        u$setMatInv(calMatInv)                  ## set matrix inverse by pssing variable CalMatInv into appropriate function     
 }
